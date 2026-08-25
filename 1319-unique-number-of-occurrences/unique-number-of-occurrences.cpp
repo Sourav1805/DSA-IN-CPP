058557135ab7ac3fc386a0076bf2127538f16bcb
Shankar_Sourav <shankarsourav236@gmail.com>
2026-08-25 19:43:01 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (46.88%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
@@ -5,13 +5,14 @@ public:
         for(int i=0;i<arr.size();i++){
             mp[arr[i]]++;
 
+
         }
-         set<int>st;
-         for(auto it:mp){
-            if(st.find(it.second)==st.end())
-            st.insert(it.second);
-            else return false;
-         }
-         return true;
+        unordered_set<int>st;
+        for(auto it:mp){
+            if(st.find(it.second)!=st.end()){
+                return false;
+            }else st.insert(it.second);
+        }
+        return true;
     }
 };
